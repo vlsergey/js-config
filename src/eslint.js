ea4c0a07f5d0f2d0b317e57790990712103eb28c
Sergey Vladimirov <vlsergey@gmail.com>
2021-03-25 18:14:40 +0300

    Lint code

--- src/eslint.js
+++ src/eslint.js
@@ -262,7 +262,7 @@ module.exports = {
   settings: {
     'import/resolver': {
       node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        extensions: [ '.js', '.jsx', '.ts', '.tsx' ],
         moduleDirectory: [ 'node_modules', 'src' ],
       },
     },
