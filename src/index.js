8af28f95d56761458f169a33f4c93219451d7f7e
Sergey Vladimirov <vlsergey@gmail.com>
2021-03-26 15:45:56 +0300

    Change index.js to CommonJS format

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-/* eslint-env node */
-/* eslint @typescript-eslint/no-unsafe-assignment: 0 */
-
-import eslint from './eslint';
-import karma from './karma';
-
-export { eslint, karma };
+/* eslint-disable */
+module.exports = {
+  eslint: require('eslint'),
+  karma: require('karma'),
+}
